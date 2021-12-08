bb96f321193706f138f0b578eecb8e1dc4411de2
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-12-08 02:22:22 +0100

    Datagrid style temporary

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,8 +1,5 @@
 import React from "react"
-
-// import { DataGrid } from "@mui/x-data-grid"
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles"
-
+import { DataGrid } from "@mui/x-data-grid"
 import AliceCarousel from "react-alice-carousel"
 
 import ButtonBarSingle from "../components/ButtonBarSingle"
@@ -20,6 +17,13 @@ const DashBoard = () => {
 		"Nantes Digital Week",
 		"WEB2DAY",
 		"DevFest",
+		"Event 5",
+		"Event 6",
+		"Event 7",
+		"Event 8",
+		"Event 9",
+		"Event 10",
+		"Event 11",
 	]
 
 	const responsive = {
@@ -34,58 +38,81 @@ const DashBoard = () => {
 	))
 
 	const columns = [
-		{ field: "id", headerName: "ID", width: 90 },
 		{
-			field: "firstName",
-			headerName: "First name",
+			field: "id",
+			headerName: "ID",
+			headerClassName: "themeHeader",
+			// width: 50,
+			renderCell: (params) => (
+				<div className="tableAvatar flex aic jcc">{params.value}</div>
+			),
+		},
+
+		{
+			field: "event",
+			headerName: "EVENT",
+			headerClassName: "themeHeader",
+			width: 350,
+			editable: true,
+		},
+		{
+			field: "team",
+			headerName: "TEAM",
+			headerClassName: "themeHeader",
+			width: 250,
+			editable: true,
+			renderCell: (params) => (
+				<div className="tableMate flex aic jcc">{params.value}</div>
+			),
+		},
+		{
+			field: "role",
+			headerName: "ROLE",
+			headerClassName: "themeHeader",
 			width: 150,
 			editable: true,
 		},
 		{
-			field: "lastName",
-			headerName: "Last name",
+			field: "actions",
+			headerName: "ACTIONS",
+			headerClassName: "themeHeader",
 			width: 150,
 			editable: true,
-		},
-		{
-			field: "age",
-			headerName: "Age",
-			type: "number",
-			width: 110,
-			editable: true,
-		},
-		{
-			field: "fullName",
-			headerName: "Full name",
-			description: "This column has a value getter and is not sortable.",
-			sortable: false,
-			width: 160,
-			valueGetter: (params) =>
-				`${params.getValue(params.id, "firstName") || ""} ${
-					params.getValue(params.id, "lastName") || ""
-				}`,
+			renderCell: (params) => <div>{params.value}</div>,
 		},
 	]
 
 	const rows = [
-		{ id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-		{ id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-		{ id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-		{ id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-		{ id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-		{ id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-		{ id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-		{ id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-		{ id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-	]
-
-	const gridtheme = createTheme({
-		overrides: {
-			root: {
-				color: "yellow",
-			},
+		{
+			id: 1,
+			event: "Printemps des fameuses",
+			team: "JD",
+			role: "Owner",
+			actions: "...",
 		},
-	})
+		{
+			id: 2,
+			event: "Nantes Digital Week",
+			team: "JD",
+			role: "Owner",
+			actions: "...",
+		},
+		{
+			id: 3,
+			event: "WEB2DAY",
+			team: ["R", "W"],
+			role: "Guest",
+			actions: "...",
+		},
+		{ id: 4, event: "DevFest", team: "A", role: "Owner" },
+		{ id: 5, event: "Event5", team: "B", role: "Guest" },
+		{ id: 6, event: "Event6", team: null, role: "Owner" },
+		{ id: 7, event: "Event7", team: "D", role: "Guest" },
+		{ id: 8, event: "Event8", team: "E", role: "Guest" },
+		{ id: 9, event: "Event9", team: "F", role: "Guest" },
+		{ id: 10, event: "Event9", team: "G", role: "Guest" },
+		{ id: 11, event: "Event9", team: "H", role: "Guest" },
+	]
 
 	return (
 		<div className="App">
@@ -111,25 +138,69 @@ const DashBoard = () => {
 
 				<h2>ALL EVENTS</h2>
 				<div className="bottomDiv flex col">
-					<div className="eventTable">
-						<div className="flex aic">Search bars</div>
-						<br></br>
-						{/* {lastEvents.map((event, index) => (
-							<div key={index} className="eventRow">
-								<h3>{event}</h3>
+					<div className="eventTable ">
+						<div className="eventSearch flex jcsb">
+							<div className="flex aic">
+								<div>Show</div>
+								<select name="eventNumber" id="eventNumber">
+									<option value={5}>5</option>
+									<option value={10}>10</option>
+									<option value={20}>20</option>
+								</select>
 							</div>
-						))} */}
+							<div className="flex aic">
+								<div>Search</div>
+								<input
+									type="text"
+									placeholder="Search"
+									onChange={(e) => console.log("search:", e.target.value)}
+								/>
+								<select name="eventNumber" id="eventNumber">
+									<option value={5}>5</option>
+									<option value={10}>10</option>
+									<option value={20}>20</option>
+								</select>
+							</div>
+						</div>
+						<br></br>
+						{/* <div>Title</div> */}
+						{/* <div>
+							{lastEvents.map((event, index) => (
+								<div key={index} className="eventRow">
+									<div>{event}</div>
+								</div>
+							))}
+						</div> */}
 
-						<MuiThemeProvider theme={gridtheme}>
-							{/* <DataGrid
-								rows={rows}
-								columns={columns}
-								pageSize={10}
-								rowsPerPageOptions={[5, 10, 20]}
-								disableSelectionOnClick
-								autoHeight={true}
-							/> */}
-						</MuiThemeProvider>
+						<DataGrid
+							rows={rows}
+							columns={columns}
+							pageSize={10}
+							// rowsPerPageOptions={[5, 10, 20]}
+							disableSelectionOnClick
+							autoHeight={true}
+							sx={{
+								color: "white",
+								justifyContent: "center",
+								"& .themeHeader": {
+									backgroundColor: "var(--dark-grey-color)",
+								},
+								"& .MuiDataGrid-columnHeader": {
+									color: "white",
+									justifyContent: "center",
+								},
+								"& .MuiDataGrid-footerContainer": {
+									backgroundColor: "green",
+									justifyContent: "space-between",
+								},
+								"& .MuiToolbar-root": {
+									color: "yellow",
+								},
+								"& .MuiSvgIcon-root": {
+									color: "white",
+								},
+							}}
+						/>
 					</div>
 				</div>
 			</div>
