7e0d72bc94067d873f40e822a931a87f2ad03595
Maxime Jezequel <maxime.jezequel@gmail.com>
2022-03-08 02:30:35 +0100

    Login/Register pages with style

--- src/screens/ManageEvents.js
+++ src/screens/ManageEvents.js
@@ -1,107 +1,139 @@
-import React from "react"
-import { useState, useRef } from "react"
-import axios from "axios"
+import React from 'react';
+import { useState, useRef } from 'react';
+import axios from 'axios';
 
-import ButtonBar from "../components/ButtonBar"
-import OptionBlock1 from "../components/ManageEvents/Options/OptionBlock1"
-import OptionBlock2 from "../components/ManageEvents/Options/OptionBlock2"
-import OptionBlock3 from "../components/ManageEvents/Options/OptionBlock3"
-import OptionBlock4 from "../components/ManageEvents/Options/OptionBlock4"
-import OptionBlock5 from "../components/ManageEvents/Options/OptionBlock5"
-import Preview from "../components/ManageEvents/Details/Preview"
-import UploadBar from "../components/ManageEvents/Details/UploadBar"
-import Watermark from "../components/ManageEvents/Details/Watermark"
+import ButtonBar from '../components/ButtonBar';
+import OptionBlock1 from '../components/ManageEvents/Options/OptionBlock1';
+import OptionBlock2 from '../components/ManageEvents/Options/OptionBlock2';
+import OptionBlock3 from '../components/ManageEvents/Options/OptionBlock3';
+import OptionBlock4 from '../components/ManageEvents/Options/OptionBlock4';
+import OptionBlock5 from '../components/ManageEvents/Options/OptionBlock5';
+import Preview from '../components/ManageEvents/Details/Preview';
+import UploadBar from '../components/ManageEvents/Details/UploadBar';
+import Watermark from '../components/ManageEvents/Details/Watermark';
 
-import image from "../assets/modele.jpg"
+import image from '../assets/modele.jpg';
 
 function ManageEvents() {
-	const [eventName, setEventName] = useState("") //event name
-	const [watermarkFile, setWatermarkFile] = useState("") //watermark filename
-	const [bubbleGridWatermark, setBubbleGridWatermark] = useState("3-3") //initial position of watermark
-	const [slider1, setSlider1] = useState(100) //percent
-	const [slider2, setSlider2] = useState(0) //percent
-	const [bubbleGridCopyright, setBubbleGridCopyright] = useState("3-1") //initial position of copyright
-	const [showCopyright, setShowCopyright] = useState(true)
+  const [eventName, setEventName] = useState(''); //event name
+  const [watermarkFile, setWatermarkFile] = useState(''); //watermark filename
+  const [bubbleGridWatermark, setBubbleGridWatermark] = useState('3-3'); //initial position of watermark
+  const [slider1, setSlider1] = useState(100); //percent
+  const [slider2, setSlider2] = useState(0); //percent
+  const [bubbleGridCopyright, setBubbleGridCopyright] = useState('3-1'); //initial position of copyright
+  const [showCopyright, setShowCopyright] = useState(true);
 
-	let max1 = 100 //maximum watermark size in percent
-	let max2 = 10 //maximum distance between watermark and edge in percent
+  let max1 = 100; //maximum watermark size in percent
+  let max2 = 10; //maximum distance between watermark and edge in percent
 
-	const inputFile = useRef(null)
+  const inputFile = useRef(null);
 
-	const handleUploadClick = () => {
-		// `current` points to the mounted file input element
-		inputFile.current.click()
-	}
+  const handleUploadClick = () => {
+    // `current` points to the mounted file input element
+    inputFile.current.click();
+  };
 
-	const handleWatermarkChange = (e) => {
-		console.log("watermark", e.target.value)
-		setWatermarkFile(e.target.value)
-	}
+  const handleWatermarkChange = (e) => {
+    console.log('watermark', e.target.value);
+    setWatermarkFile(e.target.value);
+  };
 
-	const addEvent = () => {
-		const newEvent = {
-			eventName: eventName,
-			watermarkFile: watermarkFile,
-		}
-		console.log("newEvent : ", newEvent)
-		axios.post(`${process.env.REACT_APP_URL_API}/events`, newEvent, {
-			headers: {
-				Authorization: `Basic ${localStorage.getItem("sessionToken")}`,
-			},
-		})
-	}
+  const addEvent = () => {
+    const newEventOld = {
+      eventName: eventName,
+      watermarkFile: watermarkFile,
+    };
 
-	return (
-		<div className="App">
-			<div className="bodyContainer flex col">
-				<ButtonBar handleClickSave={() => addEvent()} />
-				<h2>EVENT DETAILS</h2>
-				<div className="topDiv flex jcsb">
-					<div className="eventInfo flex col">
-						<label htmlFor="Name">Name</label>
-						<input
-							type="text"
-							placeholder="Event name"
-							className="inputEvent"
-							onChange={(e) => setEventName(e.target.value)}
-						/>
+    const newEvent = new FormData();
+    let watermarkPos = undefined;
+    this.isFolder = this.folders.length > 0 ? true : false;
+    for (let i = 0; i < document.getElementById('watermarkPos').getElementsByTagName('input').length; i++) {
+      if (document.getElementById('watermarkPos').getElementsByTagName('input')[i].checked === true) {
+        watermarkPos = document.getElementById('watermarkPos').getElementsByTagName('input')[i].value;
+      }
+    }
+    newEvent.append('eventName', eventName);
+    newEvent.append(
+      'watermark',
+      document.getElementById('eventWatermarkFile').files[0] !== undefined
+        ? document.getElementById('eventWatermarkFile').files[0]
+        : undefined
+    );
+    newEvent.append('eventWatermarkSize', document.getElementById('watermarkSize').value);
+    newEvent.append('eventWatermarkBord', document.getElementById('watermarkBord').value);
+    newEvent.append('eventWatermarkPos', watermarkPos);
+    newEvent.append('eventCredit', this.isCredit);
+    newEvent.append('eventCreditPos', this.creditPosition);
+    newEvent.append('eventCreditFont', document.getElementById('font-selector').value);
+    newEvent.append('eventCreditSize', document.getElementById('fontsize-selector').value);
+    // newEvent.append('isFolder', this.isFolder);
+    // newEvent.append('folders', this.isFolder ? this.folders : undefined);
+    newEvent.append('corrections', this.isCorrection);
 
-						<Watermark
-							slider1={slider1}
-							setSlider1={setSlider1}
-							max1={max1}
-							slider2={slider2}
-							setSlider2={setSlider2}
-							max2={max2}
-							bubbleValue={bubbleGridWatermark}
-							setBubbleValue={setBubbleGridWatermark}
-							inputFile={inputFile}
-							watermarkFile={watermarkFile}
-							setWatermarkFile={setWatermarkFile}
-							handleUploadChange={handleWatermarkChange}
-							handleUploadClick={handleUploadClick}
-						/>
-					</div>
-					<Preview image={image} />
-				</div>
-				<UploadBar />
+    newEvent.append('eventFtpHost', document.getElementById('hostInput').value);
+    newEvent.append('eventFtpUser', document.getElementById('userFTPInput').value);
+    newEvent.append('eventFtpPort', document.getElementById('portInput').value);
+    newEvent.append('eventFtpPassword', document.getElementById('passFTPInput').value);
 
-				<h2>OPTIONS</h2>
-				<div className="bottomDiv flex col">
-					<OptionBlock1
-						toggle={showCopyright}
-						setToggle={setShowCopyright}
-						bubbleValue={bubbleGridCopyright}
-						setBubbleValue={setBubbleGridCopyright}
-					/>
-					<OptionBlock2 />
-					<OptionBlock3 />
-					<OptionBlock4 />
-					<OptionBlock5 />
-				</div>
-			</div>
-		</div>
-	)
+    console.log('newEvent : ', newEvent);
+    axios.post(`${process.env.REACT_APP_URL_API}/events`, newEvent, {
+      headers: {
+        Authorization: `Basic ${localStorage.getItem('sessionToken')}`,
+      },
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="bodyContainer flex col">
+        <ButtonBar handleClickSave={() => addEvent()} />
+        <h2>EVENT DETAILS</h2>
+        <div className="topDiv flex jcsb">
+          <div className="eventInfo flex col">
+            <label htmlFor="Name">Name</label>
+            <input
+              type="text"
+              placeholder="Event name"
+              className="inputEvent"
+              onChange={(e) => setEventName(e.target.value)}
+            />
+
+            <Watermark
+              slider1={slider1}
+              setSlider1={setSlider1}
+              max1={max1}
+              slider2={slider2}
+              setSlider2={setSlider2}
+              max2={max2}
+              bubbleValue={bubbleGridWatermark}
+              setBubbleValue={setBubbleGridWatermark}
+              inputFile={inputFile}
+              watermarkFile={watermarkFile}
+              setWatermarkFile={setWatermarkFile}
+              handleUploadChange={handleWatermarkChange}
+              handleUploadClick={handleUploadClick}
+            />
+          </div>
+          <Preview image={image} />
+        </div>
+        <UploadBar />
+
+        <h2>OPTIONS</h2>
+        <div className="bottomDiv flex col">
+          <OptionBlock1
+            toggle={showCopyright}
+            setToggle={setShowCopyright}
+            bubbleValue={bubbleGridCopyright}
+            setBubbleValue={setBubbleGridCopyright}
+          />
+          <OptionBlock2 />
+          <OptionBlock3 />
+          <OptionBlock4 />
+          <OptionBlock5 />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ManageEvents
+export default ManageEvents;
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,72 +1,89 @@
-import React from "react"
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import axios from "axios"
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Snackbar, Alert } from '@mui/material';
 
-import "../App.css"
-import "../styles/Login.css"
+import '../App.css';
+import '../styles/Login.css';
 
-const Login = () => {
-	const [firstName, setFirstName] = useState("")
-	const [lastName, setLastName] = useState("")
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
+import image from '../assets/modele.jpg';
 
-	const handleClickRegister = () => {
-		const registerData = {
-			firstName: firstName,
-			lastName: lastName,
-			email: email,
-			password: password,
-		}
-		console.log(registerData)
+const Register = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-		axios.post(`${process.env.REACT_APP_URL_API}/register`, registerData)
-	}
+  const [snackOpen, setSnackOpen] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
 
-	return (
-		<div className="App">
-			<div className="bodyContainer">
-				<h2>REGISTER</h2>
-				<div className="login flex col">
-					<input
-						type="text"
-						placeholder="first name"
-						onChange={(e) => setFirstName(e.target.value)}
-					/>
+  const handleClickRegister = () => {
+    const registerData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
+    // console.log(registerData);
 
-					<input
-						type="text"
-						placeholder="last name"
-						onChange={(e) => setLastName(e.target.value)}
-					/>
+    axios
+      .post(`${process.env.REACT_APP_URL_API}/users`, registerData)
+      .then((results) => {
+        console.log(results);
+        window.location = '/login';
+      })
+      .catch((err) => {
+        // show pop up with error
+        setSnackOpen(true);
+        setErrorMsg(err.response.data);
+      });
+  };
 
-					<input
-						type="email"
-						placeholder="email"
-						onChange={(e) => setEmail(e.target.value)}
-					/>
+  const handleClose = () => {
+    setSnackOpen(false);
+  };
 
-					<input
-						type="password"
-						placeholder="password"
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-					<div className="button flex col aic jcc">
-						<button
-							className="registerBtn"
-							onClick={() => handleClickRegister()}
-						>
-							Register
-						</button>
-						<div>
-							Already have an account? <Link to="/login">Log in!</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="App">
+      <div className="bodyContainer flex">
+        <div className="login-auth-left">
+          <img src={image} alt="" />
+        </div>
+        <div className="login-auth-right login-border">
+          <h2>REGISTER</h2>
+          <div className="login flex col">
+            {/* <label htmlFor="firstName">Firstname</label> */}
+            <input type="text" placeholder="firstname" onChange={(e) => setFirstName(e.target.value)} />
+            {/* <label htmlFor="lastName">Lastname</label> */}
+            <input type="text" placeholder="lastname" onChange={(e) => setLastName(e.target.value)} />
+            {/* <label htmlFor="email">Email</label> */}
+            <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+            {/* <label htmlFor="password">Password</label> */}
+            <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+            <div className="button flex col aic jcc">
+              <button className="registerBtn" onClick={() => handleClickRegister()}>
+                Register
+              </button>
+              <Snackbar
+                open={snackOpen}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                autoHideDuration={6000}
+                onClose={() => handleClose()}
+              >
+                <Alert onClose={() => handleClose()} severity="error" sx={{ width: '100%' }}>
+                  {errorMsg.error || errorMsg}
+                </Alert>
+              </Snackbar>
+              <div>
+                Already have an account? <Link to="/login">Log in!</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Register;
